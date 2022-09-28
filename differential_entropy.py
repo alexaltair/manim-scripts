9daf31089e4f3bf8a8864810badbebaac3366341
Alex Altair <alexander.altair@gmail.com>
2022-09-28 11:20:02 -0700

    A functional animated version.

--- differential_entropy.py
+++ differential_entropy.py
@@ -6,8 +6,8 @@ Mobject.set_default(color=DARK_GRAY)
 
 class DifferentialEntropy(Scene):
     def construct(self):
-        n = 8
-        x_range_end = 10
+        n = ValueTracker(1)
+        x_range_end = 5
 
         ax = Axes(
             x_range=[0, x_range_end],
@@ -24,44 +24,61 @@ class DifferentialEntropy(Scene):
             # return -5*x + 4
             # return 1.2 - 0.2*x
 
-        some_function_curve = ax.plot(some_function, x_range=[0, x_range_end], color=BLUE_C)
+        some_function_curve = ax.plot(
+            some_function,
+            x_range=[0, x_range_end],
+            color=BLUE_C
+        )
 
+        def equal_area_rectangles():
+            dx = x_range_end/10000
+            rect_x_coords = [0]
+            sample = 0
+            area = 0
+            for i in range(int(n.get_value())):
+                while area <= (i+1)/int(n.get_value()) and sample+dx < x_range_end:
+                    area += (dx/2)*(some_function(sample) + some_function(sample+dx))
+                    sample += dx
 
-        dx = x_range_end/10000
-        rect_x_coords = [0]
-        sample = 0
-        area = 0
-        for i in range(n):
-            while area <= (i+1)/n and sample+dx < x_range_end:
-                area += (dx/2)*(some_function(sample) + some_function(sample+dx))
-                sample += dx
+                rect_x_coords.append(sample)
 
-            rect_x_coords.append(sample)
+            rect_colors = color_gradient([BLUE, GREEN], int(n.get_value()))
 
-        print(rect_x_coords)
-        rect_colors = color_gradient([BLUE, GREEN], n)
+            rectangles = []
+            for i in range(int(n.get_value())):
+                left = rect_x_coords[i]
+                right = rect_x_coords[i+1]
+                bottom = 0
+                top = 1/(int(n.get_value())*(right - left))
 
-        rectangles = []
-        for i in range(n):
-            left = rect_x_coords[i]
-            right = rect_x_coords[i+1]
-            bottom = 0
-            top = 1/(n*(right - left))
+                new_rect = Polygon(
+                    ax.coords_to_point(left, bottom),
+                    ax.coords_to_point(left, top),
+                    ax.coords_to_point(right, top),
+                    ax.coords_to_point(right, bottom),
+                )
+                new_rect.stroke_width = 1
+                new_rect.set_fill(rect_colors[i], opacity=0.5)
+                new_rect.set_stroke(GRAY)
+                rectangles.append(new_rect)
 
-            new_rect = Polygon(
-                ax.coords_to_point(left, bottom),
-                ax.coords_to_point(left, top),
-                ax.coords_to_point(right, top),
-                ax.coords_to_point(right, bottom),
-            )
-            new_rect.stroke_width = 1
-            new_rect.set_fill(rect_colors[i], opacity=0.5)
-            new_rect.set_stroke(GRAY)
-            rectangles.append(new_rect)
+            return VGroup(*rectangles)
+
+        rectangles = always_redraw(equal_area_rectangles)
+
+        n_label = always_redraw(
+            lambda: Tex(f"$n = {int(n.get_value())}$").shift(2*UP + 2*RIGHT)
+        )
 
         self.add(
             ax,
             labels,
+            n_label,
             some_function_curve,
-            *rectangles,
+            rectangles,
+        )
+
+        self.play(
+            n.animate.set_value(64 + 1),
+            run_time=8,
         )
